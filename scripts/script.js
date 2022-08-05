8e0e60f03162876d69253455b429c58204817434
ESSFIR <95650788+ESSFIR@users.noreply.github.com>
2022-08-06 01:47:19 +0300

    Animation and second section

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,39 @@ $(function(){
     $('body').toggleClass('lock');
   })
 });
+
+function animationShow(){
+  const animItems = document.querySelectorAll('.anim-items');
+  if(animItems.length > 0){
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(){
+      for(let i = 0; i < animItems.length; i++){
+        const  animItem = animItems[i];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top - 50;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if(animItemHeight > window.innerHeight){
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+          animItem.classList.add('animated');
+        } else{
+          if(!animItem.classList.contains('anim-no-hide')){
+            animItem.classList.remove('animated');
+          }
+        }
+      }
+    }
+    function offset(el){
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return {top:  rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+  }
+}
+animationShow()
+  
